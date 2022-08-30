2cb1846383e7f26e80b2797693c18983c085d652
LeonID <lmatskevich79@gmail.com>
2022-08-31 00:14:31 +0300

    feat: add logo, button, phone in header

--- webpack.config.js
+++ webpack.config.js
@@ -79,14 +79,14 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: `./css/${filename('css')}`,
     }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'src/assets'),
-          to: path.resolve(__dirname, 'dist/assets'),
-        },
-      ],
-    }),
+    // new CopyWebpackPlugin({
+    //   patterns: [
+    //     {
+    //       from: path.resolve(__dirname, 'src/assets/'),
+    //       to: path.resolve(__dirname, 'dist/assets/'),
+    //     },
+    //   ],
+    // }),
     // new ESLintPlugin(),
   ],
   devtool: isProd ? false : 'source-map',
